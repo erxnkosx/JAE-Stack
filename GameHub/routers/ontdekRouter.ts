dd4da3cdcc2659198df181a4dbebfff6f2a4bb1d
Jason Halder <86569358+JasonH418@users.noreply.github.com>
2026-05-04 12:46:53 +0200

    added paginatie

--- GameHub/routers/ontdekRouter.ts
+++ GameHub/routers/ontdekRouter.ts
@@ -3,11 +3,25 @@ import { PageInfo } from "../types";
 export function ontdekRouter() {
     const router = express.Router();
 
-    router.get("/", (req, res) => {
+    router.get("/", async (req, res) => {
         const info: PageInfo = {
             currentPage: "ontdek"
         }
-        res.render("ontdek", { info });
+        const page = Number(req.query.page) || 1;
+        const pageSize = 12;
+        const response = await fetch(
+            `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page=${page}&page_size=${pageSize}&ordering=-rating`
+        );
+
+        const data = await response.json();
+
+        res.render("ontdek", {
+            info,
+            games: data.results,
+            currentPage: page,
+            hasNextPage: Boolean(data.next),
+            hasPreviousPage: Boolean(data.previous)
+        });
     });
 
     router.get("/suggest", async (req, res) => {
